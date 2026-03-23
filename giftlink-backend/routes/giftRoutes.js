4a92564ffa8aa5014ecb1a0af32d81717d26ad22
Joshua Cui <josscui516@gmail.com>
2026-03-23 07:31:38 -0400

    Complete GiftLink backend API implementation

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,34 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const connectToDatabase = require('../models/db');
+
+// GET all gifts
 router.get('/', async (req, res) => {
     try {
-        // Task 1: Connect to MongoDB and store connection to db constant
-        // const db = {{insert code here}}
-
-        // Task 2: use the collection() method to retrieve the gift collection
-        // {{insert code here}}
-
-        // Task 3: Fetch all gifts using the collection.find method. Chain with toArray method to convert to JSON array
-        // const gifts = {{insert code here}}
-
-        // Task 4: return the gifts using the res.json method
-        res.json(/* {{insert code here}} */);
+        const db = await connectToDatabase();
+        const collection = db.collection("gifts");
+        const gifts = await collection.find({}).toArray();
+        res.json(gifts);
     } catch (e) {
         console.error('Error fetching gifts:', e);
         res.status(500).send('Error fetching gifts');
     }
 });
 
+// GET by id
 router.get('/:id', async (req, res) => {
     try {
-        // Task 1: Connect to MongoDB and store connection to db constant
-        // const db = {{insert code here}}
-
-        // Task 2: use the collection() method to retrieve the gift collection
-        // {{insert code here}}
+        const db = await connectToDatabase();
+        const collection = db.collection("gifts");
 
         const id = req.params.id;
 
-        // Task 3: Find a specific gift by ID using the collection.fineOne method and store in constant called gift
-        // {{insert code here}}
+        const gift = await collection.findOne({ id: id });
 
         if (!gift) {
             return res.status(404).send('Gift not found');
@@ -41,9 +36,7 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-
-
-// Add a new gift
+// POST
 router.post('/', async (req, res, next) => {
     try {
         const db = await connectToDatabase();
